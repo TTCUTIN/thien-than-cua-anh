a01c5bfb40a96466a359df6c87d530ed1d4cd934
TTCUTIN <khunglongongngoai@gmail.com>
2022-06-20 14:56:17 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Hí chào cậu nha :D',
+    introDesc: 'Tớ có điều này muốn hỏi, cậu nhớ phải trả lời thật lòng đó 🥺',
+    btnIntro: 'OK',
+    title: 'Cậu thích tớ phải khum 😙',
+    desc: 'Nếu cậu không trả lời hoặc thoát ra tức là cậu thích tớ đó nha :v',
+    btnYes: 'Đúm zậy, tớ thật sự thích cậu 💖',
+    btnNo: 'Cậu bị ảo tưởng à',
+    question:'Nói cho tớ lý do vì sao cậu thích tớ đi :3',
+    reasonPlaceholder: 'Trả lời',
+    btnReply: 'Gửi',
+    reply: 'Vì cậu dễ huông vlllll',
+    mess: 'Awww tớ biết mà :3',
+    messDesc: '💬💬 Còn giờ thì chần chừ gì nữa mà không nhắn tin cho tớ đi nàooooo 💬💬',
+    btnAccept: 'Okiii lunn 💖',
+    messLink: 'https://m.me/100021881253271'
+}
